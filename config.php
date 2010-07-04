872e9acc4f5b5e06827afa9a0b59bcf6645d401e
Brian Hendrickson <brian@megapump.com>
2010-07-04 12:35:35 -0700

    config

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+// http://www.facebook.com/developers/apps.php
+define(        "FB_KEY", ""      );
+define(        "FB_SEC", ""      );
+define(        "FB_AID", ""      );
+define(        "FB_NAM", ""      );
+
+// http://twitter.com/oauth_clients
+define(        "TW_KEY", ""      );
+define(        "TW_SEC", ""      );
+
+// https://www.google.com/accounts/ManageDomains
+define(        "GG_KEY", ""      );
+define(        "GG_SEC", ""      );
+
+define(       "DB_NAME", ""      );
+define(       "DB_USER", ""      );
+define(   "DB_PASSWORD", ""      );
+define( "STANDARD_LANG", "eng"   );
+define(       "DB_HOST", ""      );
+define(    "DB_CHARSET", "utf8"  );
+define(    "DB_COLLATE", ""      );
+
+
+
