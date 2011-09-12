739bbeac398b58820ef9704cbc284a2d9c26b931
Brian Hendrickson <brian@megapump.com>
2011-09-12 00:37:26 -0700

    structal

---
+++ js/controller.js
@@ -0,0 +1,23 @@
+jQuery(function($){
+
+  window.FollowButtonApp = Spine.Controller.create({
+  
+    el: $("body"),
+  
+    proxied: [],
+  
+    elements: {
+    },
+  
+    events: {
+    },
+    
+    init: function(){
+      
+    }
+
+  });
+
+  window.App = FollowButtonApp.init();
+
+});
