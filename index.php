38c6e19f09dd14c12d96586afc394b599f25b820
Brian Hendrickson <brian@megapump.com>
2011-10-31 12:08:40 -0700

    add files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 define( 'DATABASE_ENGINE',    'pgsql'); // mysql | pgsql | couchdb | mongodb | sqlite
 define( 'DATABASE_USER',      'brian');
 define( 'DATABASE_PASSWORD',  '');
-define( 'DATABASE_NAME',      'todos');
+define( 'DATABASE_NAME',      'docs');
 define( 'DATABASE_HOST',      ''); // 'localhost' | '' | IP | name
 define( 'DATABASE_PORT',      5432); // 3306/mysql | 5432/pgsql | 443
 
@@ -24,6 +24,7 @@ require 'lib/Mullet.php';
 
 
 
+
 function index() {
   require 'lib/Mustache.php';
   $m = new Mustache;
@@ -36,9 +37,6 @@ function index() {
 
 
 
-if (!in_array(strtolower($_SERVER['REQUEST_METHOD']),array('put','delete')))
-  Moor::route('/@class/@method', '@class(uc)::@method(lc)');
-Moor::route('/@class/:id([0-9A-Za-z_-]+)', '@class(uc)::'.strtolower($_SERVER['REQUEST_METHOD']));
-Moor::route('/@class', '@class(uc)::'.strtolower($_SERVER['REQUEST_METHOD']));
-Moor::route( '/', 'index' );
+
+Moor::route( '*', 'index' );
 Moor::run();
